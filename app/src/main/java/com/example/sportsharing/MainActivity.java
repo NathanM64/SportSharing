1e38fd98079fa3ef61e3781acfca92dae0d2a862
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-01-07 20:03:19 +0100

    Mise en place des commandes pour l'affichage en plein écran

--- app/src/main/java/com/example/sportsharing/MainActivity.java
+++ app/src/main/java/com/example/sportsharing/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.sportsharing;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Mise en plein écran de l'application
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
