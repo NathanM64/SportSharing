3dd70cf62fce887705347a14f678058bd133dc4f
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-01-13 23:33:46 +0100

    Création des fichiers pour le code des pages Inscription 2 et 3. Finalisation de l'inscription 2. Création et finalisation de l'inscription 3.

---
+++ app/src/main/java/com/example/sportsharing/Inscription2Activity.java
@@ -0,0 +1,13 @@
+package com.example.sportsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inscription2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription_2);
+    }
+}
