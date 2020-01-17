d44d29deb0617bd346f9b7e9cf3a928dc443c3ad
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-01-17 23:11:14 +0100

    Mise en place de la maquette Accueil (Activité)

--- app/src/main/java/com/example/sportsharing/AccueilActivite.java
+++ app/src/main/java/com/example/sportsharing/AccueilActivite.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Inscription3Activity extends AppCompatActivity {
+public class AccueilActivite extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inscription_3);
+        setContentView(R.layout.accueil_activite);
     }
-
-
 }
