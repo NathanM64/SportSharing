2a363b65a6ae8c6270c057894e713b7070c49252
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-01-19 22:40:04 +0100

    Fin de la maquette recherche_activite et mise en place de la maquette recherche_resultat

---
+++ app/src/main/java/com/example/sportsharing/RechercheResultatActivity.java
@@ -0,0 +1,13 @@
+package com.example.sportsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RechercheResultatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recherche_resultat);
+    }
+}
