f603167711cc39d980c49fd32f96882b54de8172
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-01-21 17:02:31 +0100

    Mise en place de la maquette creer_activite et réduction du placement de la barre de nav basse (85% à 90% de la fenêtre)

---
+++ app/src/main/java/com/example/sportsharing/CreerActiviteActivity.java
@@ -0,0 +1,13 @@
+package com.example.sportsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreerActiviteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creer_activite);
+    }
+}
