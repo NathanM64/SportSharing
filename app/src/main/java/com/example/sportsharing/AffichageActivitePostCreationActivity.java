de01d449a2c433261fb6d87c97372d0a21f241ce
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-02-18 19:44:45 +0100

    Mise en place de la maquette de l'affichage après la création d'une activité

---
+++ app/src/main/java/com/example/sportsharing/AffichageActivitePostCreationActivity.java
@@ -0,0 +1,14 @@
+package com.example.sportsharing;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AffichageActivitePostCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.affichage_activite_post_creation);
+    }
+}
