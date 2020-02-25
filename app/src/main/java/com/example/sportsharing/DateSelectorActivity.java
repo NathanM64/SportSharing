3cc1dc431b687aeb6bb47c4c29b65df2d5130ad6
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-02-25 16:52:17 +0100

    Mise à jour du timer et calendrier pour l'afficher sous forme de Dialog

--- app/src/main/java/com/example/sportsharing/DateSelectorActivity.java
+++ app/src/main/java/com/example/sportsharing/DateSelectorActivity.java
@@ -1,15 +1,40 @@
 package com.example.sportsharing;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.CalendarView;
 
-import android.os.Bundle;
+public class DateSelectorActivity extends Dialog {
 
-public class DateSelectorActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private CalendarView calendar;
+    private int annee, mois, jour;
+
+    public DateSelectorActivity(Context context) {
+        super(context, R.style.Theme_AppCompat_DayNight_DialogWhenLarge);
         setContentView(R.layout.date_selector);
+
+        calendar = findViewById(R.id.calendar);
+
+        calendar.setOnDateChangeListener(dateChanged);
+
+        show();
     }
+
+    public int getAnnee() { return annee; }
+
+    public int getMois() { return mois; }
+
+    public int getJour() { return jour; }
+
+    //Listener
+    CalendarView.OnDateChangeListener dateChanged = new CalendarView.OnDateChangeListener() {
+        @Override
+        public void onSelectedDayChange(CalendarView calendarView, int y, int m, int d) {
+            annee = y;
+            mois = m;
+            jour = d;
+        }
+    };
 }
 
 
--- app/src/main/java/com/example/sportsharing/TimeSelectorActivity.java
+++ app/src/main/java/com/example/sportsharing/TimeSelectorActivity.java
@@ -1,15 +1,39 @@
 package com.example.sportsharing;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.TimePicker;
 
-import android.os.Bundle;
+public class TimeSelectorActivity extends Dialog {
 
-public class TimeSelectorActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private TimePicker timer;
+    private int heure, minute;
+
+    public TimeSelectorActivity(Context context) {
+        super(context, R.style.Theme_AppCompat_DayNight_DialogWhenLarge);
         setContentView(R.layout.time_selector);
+
+        timer = findViewById(R.id.timer);
+
+        timer.setOnTimeChangedListener(timerChange);
+
+        show();
     }
+
+    public TimePicker getTimer() { return timer; }
+
+    public int getHeure() { return heure; }
+
+    public int getMinute() { return minute; }
+
+    //Listener
+    TimePicker.OnTimeChangedListener timerChange = new TimePicker.OnTimeChangedListener() {
+        @Override
+        public void onTimeChanged(TimePicker timePicker, int h, int m) {
+            heure = h;
+            minute = m;
+        }
+    };
 }
 
 
