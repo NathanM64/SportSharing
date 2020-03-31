87551fc60dbf338faa7483e5bfbe1fb0bf040295
Mathieu BOCCIARELLI <bocciarellimathieu@orange.fr>
2020-03-31 18:27:13 +0200

    Initialisation de toutes les listes d'activités de l'accueil

--- app/src/main/java/com/example/sportsharing/AccueilActivity.java
+++ app/src/main/java/com/example/sportsharing/AccueilActivity.java
@@ -40,8 +40,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
-import java.util.ArrayList;
-
 public class AccueilActivity extends AppCompatActivity {
 
     //VARIABLES Maquette générale
@@ -107,8 +105,50 @@ public class AccueilActivity extends AppCompatActivity {
         creation.setOnClickListener(createActivityListener);
         recherche.setOnClickListener(searchActivityListener);
 
+        initTabActivitesInscrite();
+        initTabActivitesTerminee();
         initTabMesActivites();
     }
+
+    public void initTabActivitesInscrite(){
+
+        ActiviteDAO activiteDAO= new ActiviteDAO(this);
+        ArrayList<Activite> activites = activiteDAO.getAllActiviteBySportifLogin(DossierVariableClasse.getInstance().utilisateur.getLogin());
+        //Si l'utilisateur possède des activitées, elle s'affiche sinon non.
+        if(!activites.isEmpty()) {
+            LayoutInflater inflate = getLayoutInflater();
+
+            TableRow mesActivites = (TableRow) inflate.inflate(R.layout.model_tablerow_accueil, null);
+
+
+            for (Activite a : activites) {
+                ((TextView) ((LinearLayout) mesActivites.getChildAt(1)).getChildAt(0)).setText(a.lieu.getVille());
+                ((TextView) ((LinearLayout) mesActivites.getChildAt(1)).getChildAt(1)).setText(a.getJour());
+            }
+            TableLayoutMyActivites.addView(mesActivites);
+        }
+    }
+
+
+    public void initTabActivitesTerminee(){
+
+        ActiviteDAO activiteDAO= new ActiviteDAO(this);
+        ArrayList<Activite> activites = activiteDAO.getAllActiviteBySportifLogin(DossierVariableClasse.getInstance().utilisateur.getLogin());
+        //Si l'utilisateur possède des activitées, elle s'affiche sinon non.
+        if(!activites.isEmpty()) {
+            LayoutInflater inflate = getLayoutInflater();
+
+            TableRow mesActivites = (TableRow) inflate.inflate(R.layout.model_tablerow_accueil, null);
+
+
+            for (Activite a : activites) {
+                ((TextView) ((LinearLayout) mesActivites.getChildAt(1)).getChildAt(0)).setText(a.lieu.getVille());
+                ((TextView) ((LinearLayout) mesActivites.getChildAt(1)).getChildAt(1)).setText(a.getJour());
+            }
+            TableLayoutMyActivites.addView(mesActivites);
+        }
+    }
+
 
     public void initTabMesActivites(){
 
